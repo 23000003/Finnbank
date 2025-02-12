a7784c00065b46569dedc080adb94cae06211d67
Gnashal <yousifceballos@gmail.com>
2025-02-12 23:20:17 +0800

    add/feat: added reg function

--- account-service/main.go
+++ account-service/main.go
@@ -1,53 +1,129 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
 	"account-service/utils"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/supabase-community/supabase-go"
 )
 
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+type Account struct {
+	Email         string    `json:"email"`
+	Full_Name     string    `json:"full_name"`
+	Phone         string    `json:"phone_number"`
+	Password      string    `json:"password"`
+	HasCard       bool      `json:"has_card"`
+	AccountNumber string    `json:"account_number"`
+	Address       string    `json:"address"`
+	Balance       float64   `json:"balance"`
+	AccountType   string    `json:"account_type"`
+	DateCreated   time.Time `json:"date_created"`
+	DateUpdated   time.Time `json:"date_updated"`
 }
 
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+func supabaseInit() *supabase.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Missing env files")
+	}
+	url := os.Getenv("DB_URL")
+	key := os.Getenv("DB_KEY")
+
+	if url == "" || key == "" {
+		log.Fatalf("Supabase URL and Keys missing")
+	}
+	client, err := supabase.NewClient(url, key, &supabase.ClientOptions{})
+	if err != nil {
+		log.Fatalf("Failed to initialize Supabase client: %v", err)
+	}
+
+	return client
 }
 
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+func getAccounts(client *supabase.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		data, count, err := client.From("account").Select("*", "exact", false).Execute()
+		if err != nil {
+			c.JSON(int(count), gin.H{"error": "Failed to fetch accounts"})
+			return
+		}
+		c.IndentedJSON(http.StatusOK, data)
+	}
+}
+
+func dataCheck(account Account) bool {
+	return account.Email != "" &&
+		account.Full_Name != "" &&
+		account.Phone != "" &&
+		account.Password != "" &&
+		account.AccountNumber != "" &&
+		account.AccountType != ""
+}
+
+func addAccount(client *supabase.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var newAcc Account
+		if err := c.ShouldBindJSON(&newAcc); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid JSON request", "details": err.Error()})
+			return
+		}
+		if !dataCheck(newAcc) {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Missing data"})
+		}
+		response, count, err := client.From("account").Insert(newAcc, false, "", "", "exact").Execute()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if count == 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No account was inserted"})
+			return
+		}
+		var insertedAcc []Account
+		err = json.Unmarshal(response, &insertedAcc)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse response " + err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Account created successfully", "account": insertedAcc})
+	}
 }
 
 func getRoot(c *gin.Context) {
-    c.String(http.StatusOK, "Welcome to the Home Page!")
+	c.String(http.StatusOK, "Welcome to the Home Page!")
 }
 
 func getHello(c *gin.Context) {
-    c.String(http.StatusOK, "Hello, World!")
+	c.String(http.StatusOK, "Hello, World!")
 }
 
 func main() {
 
-    router := gin.New()
-    logger, err := utils.NewLogger()
-    if err != nil {
-        panic(err)
-    }
-    logger.Info("Starting the application...")
+	client := supabaseInit()
+	router := gin.New()
+	logger, err := utils.NewLogger()
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Starting the application...")
 
 	serviceAPI := router.Group("/api/account-service") // base path
-    {
-        serviceAPI.GET("/", getRoot)
-        serviceAPI.GET("/albums", getAlbums)
-        serviceAPI.GET("/hello", getHello)
-    }
+	{
+		serviceAPI.GET("/", getRoot)
+		serviceAPI.GET("/accounts", getAccounts(client))
+		serviceAPI.POST("/register-acc", addAccount(client))
+		serviceAPI.GET("/hello", getHello)
+	}
 
-    if err := router.Run("localhost:8081"); err != nil {
-        logger.Fatal("Failed to start server: %v", err)
-    }
+	if err := router.Run("localhost:8081"); err != nil {
+		logger.Fatal("Failed to start server: %v", err)
+	}
 }
