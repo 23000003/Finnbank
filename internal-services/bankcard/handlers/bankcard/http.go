727bf92aaa533b9f954078e33d4d3b135ff81e24
ratilla-voltaire <07402931@usc.edu.ph>
2025-03-19 11:49:26 +0800

    I dunno!!!

--- internal-services/bankcard/handlers/bankcard/http.go
+++ internal-services/bankcard/handlers/bankcard/http.go
@@ -1,37 +1,7 @@
 package bankcard
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
 /***********
 	Use this for TESTS
 	Http Handlers for Account Service
 	This is optional since we are using grpc for communication not http
 ***********/
-
-func CreateCardHandler(w http.ResponseWriter, r *http.Request) {
-	var Request struct {
-		Account_holder string `json:"Account_holder"`
-		Account_number string `json:"Account_number"`
-		birthdate      string `json:"birthdate"`
-		uuid           string `json:"uuid"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&Request); err != nil {
-		http.Error(w, "Invalid Request", http.StatusBadRequest)
-		return
-	}
-
-	bankservice := bankcard.CreateCardHandler{}
-	newCard := bankservice.CreateCard(Request.Account_holder, Request.Account_number, Request.birthdate, Request.uuid)
-
-	if err := db.SaveCard(newCard); err != nil {
-		http.Error(w, "Failed to save card", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newCard)
-}
--- internal-services/bankcard/main.go
+++ internal-services/bankcard/main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"internal-services/bankcard/service"
-	"net/http"
+	"finnbank/common/utils"
 )
 
 func main() {
-	bankCardService := service.BankCardService{}
+	logger, err := utils.NewLogger()
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Starting the application...")
 
-	http.HandleFunc("/create-card", func(w http.ResponseWriter, r *http.Request) {
-		accountHolder := r.URL.Query().Get("accountHolder")
-		accountNumber := r.URL.Query().Get("accountNumber")
-		birthdate := r.URL.Query().Get("birthdate")
-		uuid := r.URL.Query().Get("uuid")
-
-		if accountHolder == "" || accountNumber == "" || birthdate == "" || uuid == "" {
-			http.Error(w, "Missing parameters", http.StatusBadRequest)
-			return
-		}
-
-		card := bankCardService.CreateCard(accountHolder, accountNumber, birthdate, uuid)
-		fmt.Fprintf(w, "Card created: %+v", card)
-	})
-
-	fmt.Println("BankCard service is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	RunHttpServer(logger)
 }
