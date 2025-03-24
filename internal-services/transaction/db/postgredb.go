663666ec424a6fb2216ce82b4de19f76062b2246
OppenJayme <jaymekyerivan@gmail.com>
2025-03-24 14:11:11 +0800

    add: transaction_service db

--- internal-services/transaction/db/postgredb.go
+++ internal-services/transaction/db/postgredb.go
@@ -1,8 +1,37 @@
 package db
 
+import (
+	"context"
+	"finnbank/common/utils"
+	"os"
 
-/**
-	Supabase Connection string not client for postgre db
-	since sir wants us to use use pressly for migrations
-	dont use local postgredb since it will conflict our ports and stuffs
-**/
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+func InitDb(ctx context.Context) (*pgx.Conn, error) {
+	logger, err1 := utils.NewLogger()
+	if err1 != nil {
+		panic(err1)
+	}
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Warn("Can't find Environment Variables")
+	}
+	// LOCAL_DB_URL <-- LOCAL Database
+	// ACC_DATABASE_URL <-- PROD Database
+	dbURL := os.Getenv("CONNECTION_STRING")
+	if dbURL == "" {
+		logger.Fatal("Connection string is missing")
+	}
+	logger.Info(dbURL)
+
+	conn, err := pgx.Connect(ctx, dbURL)
+	if err != nil {
+		logger.Fatal("Failed to connect to PostgreSQL: %v", err)
+		return nil, err
+	}
+	logger.Info("Successfully connected to PostgreSQL")
+	return conn, nil
+}
