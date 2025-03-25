0d43e7e729a678a2659d837ff35494ed86ce9883
Charles Benedict <charlesbenedictboquecosa@gmail.com>
2025-03-25 15:55:13 +0800

    statement proto generation

--- internal-services/statement/service/statement-service.go
+++ internal-services/statement/service/statement-service.go
@@ -1,4 +1,18 @@
 package service
 
+import (
+	"context"
+	"finnbank/common/grpc/statement"
+)
 
-type StatementService struct {}
+// type StatementService struct {
+// 	Logger *utils.Logger
+// 	Grpc   account.AccountServiceServer
+// 	account.UnimplementedAccountServiceServer
+// }
+
+
+
+func AddStatement(context.Context, *AddStatementRequest) (*AddStatementResponse, error){
+
+}
--- proto_gen.bat
+++ proto_gen.bat
@@ -1,7 +1,15 @@
 @echo off
 
-start protoc --proto_path=.protobuf ".protobuf/account.proto" ^
-    --go_out=common/grpc/account ^
+start protoc --proto_path=.protobuf ".protobuf/statement.proto" ^
+    --go_out=common/grpc/statement ^
     --go_opt=paths=source_relative ^
-    --go-grpc_out=common/grpc/account ^
+    --go-grpc_out=common/grpc/statement ^
     --go-grpc_opt=paths=source_relative
+
+REM Check if the previous command failed
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error: Failed to generate gRPC code from bankcard.proto
+    exit /b %ERRORLEVEL%
+)
+
+echo Success: gRPC code generated successfully.
