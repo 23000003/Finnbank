03a443d3b362091428fca61f311c41b0a7d023f1
OppenJayme <jaymekyerivan@gmail.com>
2025-03-26 12:21:59 +0800

    added protocol buffer transactions

--- proto_gen.bat
+++ proto_gen.bat
@@ -1,14 +1,14 @@
 @echo off
 
-protoc --proto_path=protobuf "protobuf/bankcard.proto" ^
-    --go_out=common/grpc/bankcard ^
+protoc --proto_path=.protobuf ".protobuf/transaction.proto" ^
+    --go_out=common/grpc/transaction ^
     --go_opt=paths=source_relative ^
-    --go-grpc_out=common/grpc/bankcard ^
+    --go-grpc_out=common/grpc/transaction ^
     --go-grpc_opt=paths=source_relative
 
 REM Check if the previous command failed
 IF %ERRORLEVEL% NEQ 0 (
-    echo Error: Failed to generate gRPC code from bankcard.proto
+    echo Error: Failed to generate gRPC code from transaction.proto
     exit /b %ERRORLEVEL%
 )
 
